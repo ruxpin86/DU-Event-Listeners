601e997b3ce9897e22e908642b3dc3eb2747008c
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-07-14 13:15:39 -0600

    added some queries to the utils directory on the client side will see if they work in the next few days

---
+++ client/src/utils/queries.js
@@ -0,0 +1,31 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_USERS = gql`
+  query allUsers {
+    users {
+      _id
+      username
+      events
+    }
+  }
+`;
+
+export const QUERY_SINGLE_USER = gql`
+  query singleUser($userId: ID!) {
+    user(userId: $userId) {
+      _id
+      username
+      events
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      events
+    }
+  }
+`;
