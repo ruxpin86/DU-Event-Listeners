72100b1b738c9269b428687b218aabbfe970a67a
PeterBookmyer <101828030+PeterBookmyer@users.noreply.github.com>
2022-07-14 13:41:51 -0600

    updated boilerplate header.js as well as header.css

---
+++ client/src/components/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Auth from "../../utils/auth";
+
+const Header = () => {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+  return (
+    <header className="headerEl">
+      <div className="headerDiv">
+        <Link to="/">
+          <h1 className="headerTxt" style={{ fontSize: "3rem" }}>
+            DU Event Listeners
+          </h1>
+        </Link>
+        <div>
+          {Auth.loggedIn() ? (
+            <>
+              <button className="btn btn-lg btn-light m-2" onClick={logout}>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link className="btn btn-lg btn-primary m-2" to="/login">
+                Logout
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
