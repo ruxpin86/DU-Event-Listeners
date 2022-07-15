8fd40ee06e744da9b0cdc7b6b231b1a5eec91dd9
Demon <kristyndcb@gmail.com>
2022-07-15 14:27:09 -0600

    events seeded properly

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -6,14 +6,25 @@ const eventsSeeds = require("./eventsSeeds.json");
 db.once("open", async () => {
   try {
     await User.deleteMany({});
+    await Events.deleteMany({});
     const newUsers = await User.create(userSeeds);
     const newEvents = await Events.create(eventsSeeds);
     for (let index = 0; index < newUsers.length; index++) {
       const user = newUsers[index];
       const randomNumber = Math.floor(Math.random() * 2);
-      user.update({
-        $push: { events: newEvents[randomNumber] },
-      });
+      console.log(newEvents[randomNumber]);
+      const updatedUser = await User.findByIdAndUpdate(
+        user._id,
+        {
+          $addToSet: { events: newEvents[randomNumber] },
+        },
+        {
+          new: true,
+        }
+      ).populate("events");
+      //.populate puts in everything associated with the mongo id it is referencing
+      console.log(updatedUser);
+   
     }
     console.log("all done!");
     process.exit(0);
