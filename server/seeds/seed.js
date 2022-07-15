2c64aa968a74f3d1900dc67c9e6cae8cc1211a34
Demon <kristyndcb@gmail.com>
2022-07-15 14:04:25 -0600

    created events seeds and fixed seed.js

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,11 +1,20 @@
 const db = require("../config/connection");
-const { User } = require("../models");
+const { User, Events } = require("../models");
 const userSeeds = require("./userSeeds.json");
+const eventsSeeds = require("./eventsSeeds.json");
 
 db.once("open", async () => {
   try {
     await User.deleteMany({});
-    await User.create(userSeeds);
+    const newUsers = await User.create(userSeeds);
+    const newEvents = await Events.create(eventsSeeds);
+    for (let index = 0; index < newUsers.length; index++) {
+      const user = newUsers[index];
+      const randomNumber = Math.floor(Math.random() * 2);
+      user.update({
+        $push: { events: newEvents[randomNumber] },
+      });
+    }
     console.log("all done!");
     process.exit(0);
   } catch (err) {
