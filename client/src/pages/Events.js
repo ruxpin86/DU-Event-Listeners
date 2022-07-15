c513ba8be98b8b530023dfa087fa721ab118623e
PeiYun <ollylee8520@gmail.com>
2022-07-14 21:21:56 -0600

    page transition efftct/close page to main/install react-icon

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "../style/events.css";
+import { MdClose } from "react-icons/md";
 
 export default function Events() {
   return (
-    <div>
-      <h1>Events</h1>
+    <div className="events-frame">
+      <div className="title">
+        <h1>Events</h1>
+        <a href="/main">
+          <MdClose />
+        </a>
+      </div>
     </div>
   );
 }
