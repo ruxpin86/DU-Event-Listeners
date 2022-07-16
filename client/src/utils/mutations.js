27ae8f63d7723938cb031caf1dc095f04a7ef4a5
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-07-16 15:06:04 -0600

    adjusted the backend and now users are able to sign-up and login

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String) {
+  mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
@@ -15,8 +15,8 @@ export const ADD_USER = gql`
 `;
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
       token
       user {
         _id
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -60,7 +60,7 @@ const typeDefs = gql`
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
 
-    login(email: String!, password: String!): Auth
+    login(username: String!, password: String!): Auth
 
     removeUser: User
 
