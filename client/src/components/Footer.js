079abc2f6e4d82b2ed8c4ffd5c60aa9dec746c6b
William Glynn <teddy_gram@Williams-Mac-mini.local>
2022-07-17 15:08:57 -0600

    working on adding footer icons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,23 +1,26 @@
 import React from "react";
 import "../style/footer.css";
-
+import { AiFillGithub } from "react-icons/ai";
 
 export default function () {
   return (
     <>
       <footer>
         <div className="footerEl">
-          <p>©The DU Event Listeners</p>
+          <p>©The DU Event Listeners 2022</p>
           <ul className="footer-list">
             <li>Olly</li>
             <li>Andy</li>
             <li>Kris</li>
             <li>Peter</li>
-            <li>Ted</li>
+            <li>
+              <a href="https://github.com/ruxpin86">
+                <AiFillGithub />
+              </a>
+            </li>
           </ul>
         </div>
       </footer>
-
     </>
   );
 }
