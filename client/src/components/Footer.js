3220be72b2636d03ec43ec9f0c1bd6224f9fdcd7
William Glynn <teddy_gram@Williams-Mac-mini.local>
2022-07-17 15:32:05 -0600

    added footer icons with links

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../style/footer.css";
 import { AiFillGithub } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
 
 export default function () {
   return (
@@ -9,13 +10,49 @@ export default function () {
         <div className="footerEl">
           <p>©The DU Event Listeners 2022</p>
           <ul className="footer-list">
-            <li>Olly</li>
-            <li>Andy</li>
-            <li>Kris</li>
-            <li>Peter</li>
             <li>
+              Olly
+              <a href="https://github.com/ollyhite">
+                <AiFillGithub size={20} />
+              </a>
+              <a href="https://www.linkedin.com/in/pei-yun-hite-29a3b49a/">
+                <AiFillLinkedin size={20} />
+              </a>
+            </li>
+            <li>
+              Andy
+              <a href="https://github.com/andypieratt">
+                <AiFillGithub size={20} />
+              </a>
+              <a href="https://www.linkedin.com/in/andrew-pieratt/">
+                <AiFillLinkedin size={20} />
+              </a>
+            </li>
+            <li>
+              Kris
+              <a href="https://github.com/MinnieAkuma199">
+                <AiFillGithub size={20} />
+              </a>
+              <a href="https://www.linkedin.com/in/kristyn-del-campo-banrevy-8060b2197/">
+                <AiFillLinkedin size={20} />
+              </a>
+            </li>
+            <li>
+              Peter
+              <a href="https://github.com/PeterBookmyer">
+                <AiFillGithub size={20} />
+              </a>
+              <a href="https://www.linkedin.com/in/william-glynn-71b269125/">
+                <AiFillLinkedin size={20} />
+              </a>
+            </li>
+            <li>
+              Ted
               <a href="https://github.com/ruxpin86">
-                <AiFillGithub />
+                <AiFillGithub size={20} />
+              </a>
+              <a href="https://www.linkedin.com/in/william-glynn-71b269125/">
+                <AiFillLinkedin size={20} />
               </a>
             </li>
           </ul>
